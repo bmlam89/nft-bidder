1d0246e04e5ffc37f68ae68bbf709225f7e113a9
brian m lam <brian.michael.lam@gmail.com>
2022-08-03 08:34:15 -0700

    updated db.js fron configs

--- configs/db.js
+++ configs/db.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 const connectDB = async () => {
-  let mongoUrl = process.env.MONGO_URI || 'mongodb+srv://kablam:Mongopass1!@cluster0.f3yys.mongodb.net/opensea?retryWrites=true&w=majority';
   try {
-    await mongoose.connect(mongoUrl);
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("Successfully connected to MongoDB");
   } catch (error) {
     console.log("Unable to connect to MongoDB");
