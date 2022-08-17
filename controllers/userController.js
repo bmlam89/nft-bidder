8b778d8fdc43ffcaa18ad6e42abaabdb1f57dee5
brian m lam <brian.michael.lam@gmail.com>
2022-08-16 20:38:34 -0700

    adding sweeper configs

--- controllers/userController.js
+++ controllers/userController.js
@@ -16,8 +16,6 @@ const TIME_OPTIONS = {
 const TIME_REGION = 'en-US';
 exports.addUser = async (req, res) => {
   try {
-    console.log(req.body,'REQ BODY');
-
     let user = await User.findOne({'address': req.body.address}).exec();
    
     if(!user) {
@@ -64,6 +62,36 @@ exports.login = async (req, res) => {
     res.status(500).json({
       status: "error",
       message: "userController error when trying to login",
+    });
+  }
+}
+
+exports.submitConfigs = async (req, res) => {
+  try {
+    req.body.configs.map(async (config) => {
+      let { start, nextSweep, expiration } = helpers.formatSweeperTimestamps(config);
+      config.start = start.toLocaleString();
+      config.nextSweep = nextSweep.toLocaleString(TIME_REGION, TIME_OPTIONS); 
+      config.expiration = expiration.toLocaleString(TIME_REGION, TIME_OPTIONS);
+      return config;
+    });
+
+    let query = { address: req.body.address };
+    let update = { 
+      configs: req.body.configs, 
+      updatedAt: (new Date()).toLocaleString(TIME_REGION, TIME_OPTIONS)
+    };
+    
+    let doc = await User.findOneAndUpdate(query, update);
+    console.log(doc,'docccc');
+    res.status(200).json({
+      status: "success",
+      user: doc
+    });  } catch(e) {
+    console.error(e);
+    res.status(500).json({
+      status: "error",
+      message: "userController error when trying to submit sweeper configs",
     });
   }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/userController');
 
 router.route("/register").post(userController.addUser);
 router.route('/login').post(userController.login);
+router.route('/submit').post(userController.submitConfigs);
 //router.route("/top").get(userController.getTopCollections);
 //router.route('/details/:slug').get(userController.getCollectionDetails);
 
